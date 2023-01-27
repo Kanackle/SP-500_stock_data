875ce02a28cac87c81ee2d7ac8eb28faf7d8c208
nrpineda <new.lifenrpn@gmail.com>
2023-01-27 03:15:43 -0800

    Adding Queries.sql file to Database folder

---
+++ Database/Queries.sql
@@ -0,0 +1,10 @@
+-- QUERIES
+
+-- To better understand if there is any relationship in between the volume and closing price on
+-- the Stock Data, we will create a table that only contains the following columns:
+-- Close, Rec_Date, Volume, Name, and Symbol.
+
+SELECT sd.close, sd.rec_date, sd.volume, cd.name, cd.symbol
+FROM stock_data as sd
+JOIN company_data as cd
+ON cd.symbol = sd.symbol;
